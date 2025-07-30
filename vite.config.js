b91ba381c208c6d3d7e0e41760e99c91a579c188
USER <user@usernoMacBook-Air.local>
2025-07-30 14:17:17 +0900

    fix: allow render host

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,9 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: process.env.PORT || 3000,
+    allowedHosts: ['access-relay.onrender.com']
   },
   preview: {
-    allowedHosts: ['access-relay.onrender.com'],
-  },
+    allowedHosts: ['access-relay.onrender.com']
+  }
 });
